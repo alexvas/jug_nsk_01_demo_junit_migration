1a06893c7aefb3c542bb6a00c170a291cff6a97f
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-18 17:26:46 +0700

    тупо включаем настройку per_class

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ include(
         "01_junit4_java",
         "02_junit4_kotlin",
         "03_junit4_kotlin_jvmstatic",
-        "04_junit4_on_junit5_platform"
+        "04_junit4_on_junit5_platform",
+        "05_junit4_on_junit5_platform_per_class"
 )
 
