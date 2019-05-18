73e5adb90b612f34f5231b73e06b2a1b650a455d
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-18 11:21:26 +0700

    апнул версию плагина Котлина
    с ней версия jvm-платформы по умолчанию равна 1.8, явно указывать её не
    нужно

--- 03_junit4_kotlin_jvmstatic/build.gradle.kts
+++ 03_junit4_kotlin_jvmstatic/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm")
 }
@@ -9,13 +7,4 @@ dependencies {
     // Use JUnit test framework
     testImplementation("junit:junit:4.12")
     implementation(kotlin("stdlib-jdk8"))
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
