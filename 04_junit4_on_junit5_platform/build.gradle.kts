5af44a665542dcb43a8cac09572a167d1af92af0
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-18 12:06:23 +0700

    пускаем junit4 через винтажную платформу junit5

--- 04_junit4_on_junit5_platform/build.gradle.kts
+++ 04_junit4_on_junit5_platform/build.gradle.kts
@@ -2,9 +2,20 @@ plugins {
     kotlin("jvm")
 }
 
-dependencies {
+tasks.test {
+    useJUnitPlatform()
+}
 
-    // Use JUnit test framework
-    testImplementation("junit:junit:4.12")
+dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("junit:junit:4.12")
+
+    val junitBomVersion = "5.4.2"
+    implementation(enforcedPlatform("org.junit:junit-bom:$junitBomVersion")) {
+        because("enforce matching Platform, Jupiter, and Vintage versions")
+    }
+
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
