336ef9239fab86274841e233d80a8ccc4f4fd2fb
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-18 19:25:41 +0700

    добавил примеры JUnit 3, 4, 5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include(
         "02_junit4_kotlin",
         "03_junit4_kotlin_jvmstatic",
         "04_junit4_on_junit5_platform",
-        "05_junit4_on_junit5_platform_per_class"
+        "05_junit4_on_junit5_platform_per_class",
+        "06_junit5_platform_spectrum"
 )
 
