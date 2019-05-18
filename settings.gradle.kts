9ad9f1dacd245ed7dd6cf131cb9b4d7fae847128
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-18 11:33:29 +0700

    копипаст предыдущего шага

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "demo_junit_migration"
 include(
         "01_junit4_java",
         "02_junit4_kotlin",
-        "03_junit4_kotlin_jvmstatic"
+        "03_junit4_kotlin_jvmstatic",
+        "04_junit4_on_junit5_platform"
 )
 
