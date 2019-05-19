a11bf0a0ab499de47360993b2b2744b553db77b9
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-19 12:32:09 +0700

    удалил ненужный сборочный скрипт
    и подправил остальные

--- 02_junit4_java/build.gradle.kts
+++ 02_junit4_java/build.gradle.kts
@@ -1,5 +1,6 @@
 
 dependencies {
     implementation(project(":01_library_under_test"))
+    
     testImplementation("junit:junit:4.12")
 }
--- 03_junit4_kotlin/build.gradle.kts
+++ 03_junit4_kotlin/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":01_library_under_test"))
-
-    // Use JUnit test framework
-    testImplementation("junit:junit:4.12")
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("junit:junit:4.12")
 }
