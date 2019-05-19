60d65a24e0b804de822467ca0375fbff141b673d
a.a.vasiljev <a.a.vasiljev@gmail.com>
2019-05-19 12:07:08 +0700

    сдвинул названия директорий
    01 -> 02,
    02 -> 03,
    ...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 rootProject.name = "demo_junit_migration"
 
 include(
-        "01_junit4_java",
-        "02_junit4_kotlin",
-        "03_junit4_kotlin_jvmstatic",
-        "04_junit4_on_junit5_platform",
-        "05_junit4_on_junit5_platform_per_class",
-        "06_junit5_platform_spectrum"
+        "02_junit4_java",
+        "03_junit4_kotlin",
+        "04_junit4_kotlin_jvmstatic",
+        "05_junit4_on_junit5_platform",
+        "06_junit4_on_junit5_platform_per_class",
+        "07_junit5_platform_spectrum"
 )
 
