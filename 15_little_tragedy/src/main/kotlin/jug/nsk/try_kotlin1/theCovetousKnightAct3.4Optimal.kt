af233fae35737ecca0bb1831883dfc65ddf4b65b
a.a.vasiljev <a.a.vasiljev@gmail.com>
2020-01-18 21:49:36 +0700

    оптимальный сценарий заполнения сундуков
    при структурной параллельности. Вкладываем рамку структурной параллельности внутрь try {} catch, чтобы не упустить ни единой ошибки.

---
+++ 15_little_tragedy/src/main/kotlin/jug/nsk/try_kotlin1/theCovetousKnightAct3.4Optimal.kt
@@ -0,0 +1,63 @@
+package jug.nsk.try_kotlin1
+
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.function.Supplier
+
+class OptimalVault(
+        private val farm: Supplier<Int>,
+        private val chestFactory: Supplier<Chest>,
+        initialChests: List<Chest>
+) {
+    private val chests = initialChests.toMutableList()
+
+    suspend fun saveHandfulOfGold(amount: Int) = try {
+
+        withContext(Dispatchers.Default) {
+            val lastChest = chests.last()
+            val leftInLastChest = CHEST_SIZE - lastChest.count()
+            if (amount <= leftInLastChest) {
+                // всё помещается в последний сундук, майним монетки параллельно
+                repeat(amount) {
+                    launch {
+                        val coin = farm.get()
+                        lastChest.put(coin)
+                    }
+                }
+                return@withContext
+            }
+            // заполняем сундуки параллельно
+            bgFill(lastChest, leftInLastChest)
+            val amountLeft = amount - leftInLastChest
+            repeat(amountLeft / CHEST_SIZE) {
+                // эти сундуки надо заполнить доверху
+                bgFill(newChest(), CHEST_SIZE)
+            }
+            val stillLeft = amountLeft % CHEST_SIZE
+            if (stillLeft > 0) {
+                newChest().fill(stillLeft)
+            }
+        }
+    } catch (t: Throwable) {
+        System.err.println("failed to save gold:")
+        t.printStackTrace(System.err)
+    }
+
+    private fun newChest(): Chest {
+        val newChest = chestFactory.get()
+        chests.add(newChest)
+        return newChest
+    }
+
+    private fun Chest.fill(amount: Int) = repeat(amount) {
+        val coin = farm.get()
+        put(coin)
+    }
+
+    private fun CoroutineScope.bgFill(chest: Chest, amount: Int) = launch { chest.fill(amount) }
+
+    fun count() = chests.sumBy { it.count() }
+}
---
+++ 15_little_tragedy/src/test/kotlin/jug/nsk/try_kotlin1/Act3.4OptimalTest.kt
@@ -0,0 +1,32 @@
+@file:Suppress("NonAsciiCharacters", "ClassName")
+
+package jug.nsk.try_kotlin1
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
+
+
+class Act3_4OptimalTest {
+    @Test
+    fun `проверяем, что всё сошлось`() {
+
+        val toAdd = 100500
+        val farm = CoinFarm()
+
+        val vault = OptimalVault(farm, AsyncChest, initialAsyncChests())
+        val startCount = vault.count()
+
+        runBlocking {
+            val elapsed = measureTimeMillis {
+                vault.saveHandfulOfGold(toAdd)
+            }
+            println("saved $toAdd coins in $elapsed milliseconds")
+        }
+
+        assertEquals(toAdd, farm.count(), "как заказывали, так и сгенерировали")
+        assertEquals(startCount + toAdd, vault.count(), "Ключи, ключи мои!...")
+    }
+
+}
